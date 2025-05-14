da0c47777d929fd73b6556a5dd228fe9035084c1
Matrix3923r45 <matrix24221@gmail.com>
2025-05-14 15:24:50 +0530

    Add files via upload

---
+++ lavalink.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    enabled: true, 
+    lavalink: {
+        name: "DevinePixel",
+        password: "devinepixy",
+        host: "193.226.78.187",
+        port:  3543,
+        secure: false
+    }
+};
